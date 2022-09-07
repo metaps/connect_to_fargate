8ad66198dec08aa5a69e88eba9c4c2e621576a35
KODA Hiroshi <koda@metaps-payment.com>
2022-09-08 01:48:59 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,232 @@
+# Fargate接続ツールの説明
+
+## 事前準備
+- クライアント側でECS Execを利用可能にしておく
+- 参考情報
+  - https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/userguide/ecs-exec.html
+  - https://docs.aws.amazon.com/ja_jp/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html
+
+
+## 利用方法
+```
+# Profileを設定して、sso login する
+export AWS_PROFILE=profile_name; aws sso login
+(url をブラウザで参照し、 codeを入力する)
+
+# スクリプト実行する
+connect_to_fargate.py
+
+※/usr/local/binに配置
+※connect_to_fargate.py_(日時).logにログが出力されます。
+
+※引数に以下を利用できるように追加しました。
+--cluster=クラスター名（指定ない場合は、対話型の選択画面に遷移）
+--service=サービス名（指定ない場合は、対話型の選択画面に遷移）
+--task=タスク名（指定ない場合は、対話型の選択画面に遷移）
+--container=コンテナ名（指定ない場合は、対話型の選択画面に遷移）
+--cmd=コンテナで実行するコマンド（指定ない場合は、/bin/bash）
+
+※Ctrl + Cの挙動は変更しました（接続処理直前にSIGINTをSIG_IGNに変換する処理を追加）
+```
+
+### 実行結果（例１ 引数なし）
+```
+$ connect_to_fargate.py
+処理を開始します
+[?] 接続先が存在するクラスター名を選択してください: default
+ ❯ default
+   cluster_a
+   cluster_b
+
+クラスター名: default
+
+[?] 接続先が存在するサービス名を選択してください: ticketpay-wordpress-development
+ ❯ service_a
+   service_b
+
+サービス名: service_a
+
+[?] 接続先が存在するタスク名を選択してください: 779933414f454c76960d15b37ab15f26
+ ❯ 779933414f454c76960d15b37ab15f26
+
+タスク名: 779933414f454c76960d15b37ab15f26
+
+[?] 接続先のコンテナ名を選択してください: app
+ ❯ app
+   web
+
+コンテナ名: app
+
+以下のFargateに接続します
+----------------------------------------
+クラスター名: default
+サービス名: service_a
+タスク名: 779933414f454c76960d15b37ab15f26
+コンテナ名: app
+----------------------------------------
+
+[?] こちらに接続してよろしいですか: yes
+ ❯ yes
+   no
+
+Fargateにログインします
+
+The Session Manager plugin was installed successfully. Use the AWS CLI to start a session.
+
+
+Starting session with SessionId: ecs-execute-command-08107c57e1eb5fee9
+root@ip-172-30-2-204:/#
+↑コンテナにログインしている
+exit
+
+
+Exiting session with sessionId: ecs-execute-command-08107c57e1eb5fee9.
+
+CompletedProcess(args='/usr/local/bin/aws ecs execute-command --cluster fumizawa-test-cluster --task 87b5a48c8b99450d9dea5443c863ee5d --container nginx --interactive --command /bin/bash | tee ./connect_to_fargate.py_20220629090944872720.log', returncode=0)
+Fargateからログアウトしました
+```
+
+### 実行結果（例２ 一部引数あり）
+```
+$ connect_to_fargate.py --cluster=default --container=web
+処理を開始します
+[?] 接続先が存在するサービス名を選択してください: ticketpay-wordpress-development
+ ❯ service_a
+   service_b
+
+サービス名: service_a
+
+[?] 接続先が存在するタスク名を選択してください: 779933414f454c76960d15b37ab15f26
+ ❯ 779933414f454c76960d15b37ab15f26
+
+タスク名: 779933414f454c76960d15b37ab15f26
+
+以下のFargateに接続します
+----------------------------------------
+クラスター名: default
+サービス名: service_a
+タスク名: ae95d0c222424643be7ddd8e288b1229
+コンテナ名: web
+----------------------------------------
+
+[?] こちらに接続してよろしいですか: yes
+ ❯ yes
+   no
+
+Fargateにログインします
+（略）
+```
+
+### 実行結果（例３ fargatessh利用）
+```
+$ fargatessh -p profile -c default -t app -f
+
+処理を開始します
+[?] 接続先が存在するサービス名を選択してください: ticketpay-wordpress-development
+ ❯ service_a
+   service_b
+
+サービス名: service_a
+
+[?] 接続先が存在するタスク名を選択してください: 779933414f454c76960d15b37ab15f26
+ ❯ 779933414f454c76960d15b37ab15f26
+
+タスク名: 779933414f454c76960d15b37ab15f26
+
+以下のFargateに接続します
+----------------------------------------
+クラスター名: default
+サービス名: service_a
+タスク名: ae95d0c222424643be7ddd8e288b1229
+コンテナ名: web
+----------------------------------------
+
+Fargateにログインします
+（略）
+```
+
+## トラブルシューティング
+'ServiceNotActiveException'が発生した場合
+以下を実行して新しいタスクに入れ替える
+```
+aws ecs update-service \
+--cluster (クラスター名) \ 
+--service (サービス名) \
+--enable-execute-command
+'TargetNotConnectedException'が発生した場合
+正しいセキュリティグループにしているか、ネットワーク経路が確保されているかを確認する
+```
+
+### 汎用的な調査方法
+https://github.com/aws-containers/amazon-ecs-exec-checker
+check-ecs-execを実行する
+```
+[実行例]
+$ check-ecs-exec.sh (クラスター名) (タスク名)
+※/usr/local/binに配置しているため
+
+[実行結果]
+-------------------------------------------------------------
+Prerequisites for check-ecs-exec.sh v0.7
+-------------------------------------------------------------
+  jq      | OK (/usr/bin/jq)
+  AWS CLI | OK (/usr/local/bin/aws)
+
+-------------------------------------------------------------
+Prerequisites for the AWS CLI to use ECS Exec
+-------------------------------------------------------------
+  AWS CLI Version        | OK (aws-cli/2.6.3 Python/3.9.11 Linux/5.10.118-111.515.amzn2.x86_64 exe/x86_64.amzn.2 prompt/off)
+  Session Manager Plugin | OK (1.2.339.0)
+
+-------------------------------------------------------------
+Checks on ECS task and other resources
+-------------------------------------------------------------
+Region : ap-northeast-1
+Cluster: (クラスター名)
+Task   : (タスク名)
+-------------------------------------------------------------
+  Cluster Configuration  | Audit Logging Not Configured
+  Can I ExecuteCommand?  | arn:aws:iam::172972874842:role/aws-reserved/sso.amazonaws.com/ap-northeast-1/AWSReservedSSO_Administrators@FrontSystem_10607a14223798a0
+     ecs:ExecuteCommand: allowed
+     ssm:StartSession denied?: allowed
+  Task Status            | RUNNING
+  Launch Type            | Fargate
+  Platform Version       | 1.4.0
+  Exec Enabled for Task  | NO　　　←今回の実行例の場合はこれが原因
+  Container-Level Checks |
+    ----------
+      Managed Agent Status - SKIPPED
+    ----------
+    ----------
+      Init Process Enabled ((タスク名):(タスクのバージョン))
+    ----------
+         1. Disabled - "web"
+         2. Disabled - "app"
+    ----------
+      Read-Only Root Filesystem ((タスク名):(タスクのバージョン))
+    ----------
+         1. Disabled - "web"
+         2. Disabled - "app"
+  Task Role Permissions  | arn:aws:iam::172972874842:role/ecsTaskRole
+     ssmmessages:CreateControlChannel: allowed
+     ssmmessages:CreateDataChannel: allowed
+     ssmmessages:OpenControlChannel: allowed
+     ssmmessages:OpenDataChannel: allowed
+  VPC Endpoints          |
+    Found existing endpoints for vpc-01cd9e8961a6cca07:
+      - com.amazonaws.ap-northeast-1.s3
+      - com.amazonaws.ap-northeast-1.kms
+      - com.amazonaws.ap-northeast-1.ec2messages
+      - com.amazonaws.ap-northeast-1.ssm
+      - com.amazonaws.ap-northeast-1.ssmmessages
+      - com.amazonaws.ap-northeast-1.secretsmanager
+  Environment Variables  | (kaihipay-wordpress-development:37)
+       1. container "web"
+       - AWS_ACCESS_KEY: not defined
+       - AWS_ACCESS_KEY_ID: not defined
+       - AWS_SECRET_ACCESS_KEY: not defined
+       2. container "app"
+       - AWS_ACCESS_KEY: not defined
+       - AWS_ACCESS_KEY_ID: not defined
+       - AWS_SECRET_ACCESS_KEY: not defined
+ ```
---
+++ connect_to_fargate.py
@@ -0,0 +1,387 @@
+#!/usr/bin/env python3
+import boto3
+import sys
+import subprocess
+import os
+import traceback
+import logging
+import datetime
+import signal
+import inquirer
+import json
+import string
+from inquirer import themes
+from inquirer.render.console import ConsoleRender, List
+from readchar import key
+
+# emacs風のキーバインド
+class ExtendedConsoleRender(ConsoleRender):
+    def render_factory(self, question_type):
+        if question_type == "list":
+            return ExtendedList
+        return super().render_factory(question_type)
+
+# CTRL_MAP["B"]はkey.CTRL_Bでも良いのだけれどAからZまで全部は定義されていなかったので
+CTRL_MAP = {c: chr(i) for i, c in enumerate(string.ascii_uppercase, 1)}
+
+
+class ExtendedList(List):
+    def process_input(self, pressed):
+        # emacs style
+        if pressed in (CTRL_MAP["B"], CTRL_MAP["P"]):
+            pressed = key.UP
+        elif pressed in (CTRL_MAP["F"], CTRL_MAP["N"]):
+            pressed = key.DOWN
+        elif pressed == CTRL_MAP["G"]:
+            pressed = CTRL_MAP["C"]
+        elif pressed == CTRL_MAP["A"]:
+            self.current = 0
+            return
+        elif pressed == CTRL_MAP["G"]:
+            self.current = len(self.question.choices) - 1
+            return
+
+        # vi style
+        if pressed in ("k", "h"):
+            pressed = key.UP
+        elif pressed in ("j", "l"):
+            pressed = key.DOWN
+        elif pressed == "q":
+            pressed = key.CTRL_C
+
+        # effect (rendering)
+        super().process_input(pressed)
+
+# ログ出力設定関数
+def setLogger():
+  script_name = __file__.split('/')[-1]
+  dt = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')
+  logfile_name = './{}_{}.log'.format(script_name, dt)
+
+  logger = logging.getLogger(script_name)
+  logger.setLevel(logging.INFO)
+
+  fmt = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+  handler = logging.FileHandler(logfile_name)
+  handler.setLevel(logging.INFO)
+  handler.setFormatter(fmt)
+  logger.addHandler(handler)
+
+  fmt_stdout = logging.Formatter('%(message)s')
+  handler_stdout= logging.StreamHandler()
+  handler_stdout.setLevel(logging.INFO)
+  handler_stdout.setFormatter(fmt_stdout)
+  logger.addHandler(handler_stdout)
+
+  return logger, logfile_name
+
+def selected_answer(choices, message):
+  questions = [
+      inquirer.List(
+          "answer",
+          message=message,
+          choices=choices,
+          carousel=True,
+      )
+  ]
+  answer = inquirer.prompt(questions, render=ExtendedConsoleRender(theme=themes.GreenPassion()))
+  return json.loads(json.dumps(answer))['answer']
+
+
+# クラスター名のチェック
+def checkCluster(cluster_name):
+  session = boto3.session.Session(profile_name = os.environ['AWS_PROFILE'])
+  ecs = session.client('ecs')
+
+  cluster_list = []
+  for clusterArn in ecs.list_clusters()['clusterArns']:
+    cluster = clusterArn.split('/')[len(clusterArn.split('/')) - 1]
+    cluster_list.append(cluster)
+
+  if cluster_name in cluster_list:
+    return True
+  else :
+    return False
+
+# クラスター名の設定
+def setCluster(logger):
+  session = boto3.session.Session(profile_name = os.environ['AWS_PROFILE'])
+  ecs = session.client('ecs')
+
+  cluster_list = []
+  for clusterArn in ecs.list_clusters()['clusterArns']:
+    cluster = clusterArn.split('/')[len(clusterArn.split('/')) - 1]
+    cluster_list.append(cluster)
+
+  cluster_name = selected_answer(cluster_list, "接続先が存在するクラスター名を選択してください")
+
+  if checkCluster(cluster_name):
+    logger.info('クラスター名: {}\n'.format(cluster_name))
+    return cluster_name
+  else :
+    raise Exception('正しいクラスター名を選択してください。')
+
+# サービス名のチェック
+def checkService(cluster_name, service_name):
+  session = boto3.session.Session(profile_name = os.environ['AWS_PROFILE'])
+  ecs = session.client('ecs')
+
+  service_list = []
+  for serviceArn in ecs.list_services(
+    cluster = cluster_name,
+    launchType = 'FARGATE'
+  )['serviceArns']:
+    service = serviceArn.split('/')[len(serviceArn.split('/')) - 1]
+    service_list.append(service)
+
+  if service_name in service_list:
+    return True
+  else :
+    return False
+
+# サービス名の設定
+def setService(logger, cluster_name):
+  session = boto3.session.Session(profile_name = os.environ['AWS_PROFILE'])
+  ecs = session.client('ecs')
+
+  service_list = []
+  for serviceArn in ecs.list_services(
+    cluster = cluster_name,
+    launchType = 'FARGATE'
+  )['serviceArns']:
+    service = serviceArn.split('/')[len(serviceArn.split('/')) - 1]
+    service_list.append(service)
+
+  service_name = selected_answer(service_list, "接続先が存在するサービス名を選択してください")
+  if checkService(cluster_name, service_name):
+    logger.info('サービス名: {}\n'.format(service_name))
+    return service_name
+  else :
+    raise Exception('正しいサービス名を選択してください。')
+
+# タスク名のチェック
+def checkTask(cluster_name, service_name, task_name):
+  session = boto3.session.Session(profile_name = os.environ['AWS_PROFILE'])
+  ecs = session.client('ecs')
+
+  task_list = []
+  for taskArn in ecs.list_tasks(
+    cluster = cluster_name,
+    serviceName = service_name,
+    desiredStatus = 'RUNNING',
+    launchType = 'FARGATE'
+  )['taskArns']:
+    task = taskArn.split('/')[len(taskArn.split('/')) - 1]
+    task_list.append(task)
+
+  if task_name in task_list:
+    return True
+  else :
+    return False
+
+# タスク名の設定
+def setTask(logger, cluster_name, service_name):
+  session = boto3.session.Session(profile_name = os.environ['AWS_PROFILE'])
+  ecs = session.client('ecs')
+
+  task_list = []
+  for taskArn in ecs.list_tasks(
+    cluster = cluster_name,
+    serviceName = service_name,
+    desiredStatus = 'RUNNING',
+    launchType = 'FARGATE'
+  )['taskArns']:
+    task = taskArn.split('/')[len(taskArn.split('/')) - 1]
+    task_list.append(task)
+  task_name = selected_answer(task_list, "接続先が存在するタスク名を選択してください")
+
+  if checkTask(cluster_name, service_name, task_name):
+    logger.info('タスク名: {}\n'.format(task_name))
+    return task_name
+  else :
+    raise Exceptiont('正しいタスク名を選択してください。')
+
+# コンテナ名のチェック
+def checkContainer(cluster_name, task_name, container_name):
+  session = boto3.session.Session(profile_name = os.environ['AWS_PROFILE'])
+  ecs = session.client('ecs')
+
+  task_detail_list = ecs.describe_tasks(
+    cluster = cluster_name,
+    tasks=[
+      task_name
+    ],
+  )
+  container_name_list = []
+  for task in task_detail_list['tasks']:
+    for container in task['overrides']['containerOverrides']:
+      container_name_list.append(container['name'])
+
+  if container_name in container_name_list:
+    return True
+  else :
+    return False
+
+# コンテナ名の設定
+def setContainer(logger, cluster_name, task_name):
+  session = boto3.session.Session(profile_name = os.environ['AWS_PROFILE'])
+  ecs = session.client('ecs')
+
+  container_list = []
+  task_detail_list = ecs.describe_tasks(
+    cluster = cluster_name,
+    tasks=[
+      task_name
+    ],
+  )
+  for task in task_detail_list['tasks']:
+    for container in task['overrides']['containerOverrides']:
+      container_list.append(container['name'])
+
+  container_name = selected_answer(container_list, "接続先のコンテナ名を選択してください")
+
+  if checkContainer(cluster_name, task_name, container_name):
+    logger.info('コンテナ名: {}\n'.format(container_name))
+    return container_name
+  else :
+    raise Exception('正しいコンテナ名を選択してください。')
+
+# FARGATEへ接続
+def ecsExecute(logger, cluster_name, service_name, task_name, container_name, shell_cmd, logfile, force_connect):
+  ## 接続先確認のメッセージを出力
+  str  = '以下のFargateに接続します\n'
+  str += '----------------------------------------\n'
+  str += 'クラスター名: {}\n'.format(cluster_name)
+  str += 'サービス名: {}\n'.format(service_name)
+  str += 'タスク名: {}\n'.format(task_name)
+  str += 'コンテナ名: {}\n'.format(container_name)
+  str += '----------------------------------------\n'
+  logger.info(str)
+  if force_connect == False:
+    is_exec= selected_answer(['yes', 'no'], "こちらに接続してよろしいですか")
+
+  if force_connect == True or is_exec.startswith('y'):
+    #session = boto3.session.Session(profile_name = os.environ['AWS_PROFILE'])
+    #ecs = session.client('ecs')
+    #ecs.execute_command(
+    #  cluster = cluster_name,
+    #  container = container_name,
+    #  command = '/bin/bash',
+    #  interactive = True,
+    #  task = task_name
+    #)
+    #/bin/bashの場合セッションが切れてしまうためsubprocessを利用する方式に変更
+    logger.info('Fargateにログインします')
+    cmd  = '/usr/local/bin/aws ecs execute-command '
+    cmd += '--cluster {} '.format(cluster_name)
+    cmd += '--task {} '.format(task_name)
+    cmd += '--container {} '.format(container_name)
+    cmd += '--interactive --command {} | tee {}'.format(shell_cmd, logfile)
+
+    ## Ctrl+C(SIGINTシグナル)を無視
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+    ## subprocess実行
+    out = subprocess.run(cmd, text=True, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr, shell=True)
+    logger.info(out)
+    logger.info('Fargateからログアウトしました')
+  return
+
+def view_help():
+  message = """
+connect_to_fargate.py
+
+Usage:
+1. AWS_PROFILE を設定
+  e.g.) export AWS_PROFILE=mpfront
+2. sso login を行う
+  aws sso login
+3. コマンド実行
+  connect_to_fargate.py [arguments]
+
+Arguments:
+   --help|-h                    help表示
+   --cluster=<cluster_name>     クラスター名指定
+   --service=<service_name>     サービス名指定
+   --task=<task_name>           タスク名指定
+   --container=<container_name> コンテナ名指定
+   --cmd=<command>              コンテナで実行するコマンド（指定ない場合は、/bin/bash）
+   --force                      接続確認なしでログインを行う
+""".strip()
+
+  print(message)
+
+# 主処理
+def main():
+  try:
+    logger, logfile = setLogger()
+
+    ## 初期値の定義
+    cluster_name, service_name, task_name, container_name = '', '', '', ''
+    shell_cmd       = '/bin/bash'
+
+    force_connect = False
+    ## 引数の格納
+    for a in sys.argv:
+      if a.startswith('--cluster='):
+        cluster_name = a.replace('--cluster=', '').strip('\'" []')
+      elif a.startswith('--service='):
+        service_name = a.replace('--service=', '').strip('\'" []')
+      elif a.startswith('--task='):
+        task_name = a.replace('--task=', '').strip('\'" []')
+      elif a.startswith('--container='):
+        container_name = a.replace('--container=', '').strip('\'" []')
+      elif a.startswith('--cmd='):
+        shell_cmd = a.replace('--cmd=', '').strip('\'" []')
+      elif a.startswith('--force'):
+        force_connect = True
+      if a.startswith('--help') or a.startswith('-h'):
+        view_help()
+        return
+    logger.info('処理を開始します')
+
+    ## 引数で指定がない場合に設定する関数を実行する
+    if cluster_name == '':
+      cluster_name    = setCluster(logger)
+
+    if service_name == '':
+      if checkCluster(cluster_name):
+        service_name    = setService(logger, cluster_name)
+      else :
+        raise Exception('正しいクラスター名を指定してください。')
+
+    if task_name == '':
+      if checkCluster(cluster_name) and \
+         checkService(cluster_name, service_name):
+        task_name       = setTask(logger, cluster_name, service_name)
+      else :
+        raise Exception('正しいクラスター名またはサービス名を指定してください。')
+
+    if container_name == '':
+      if checkCluster(cluster_name) and \
+         checkService(cluster_name, service_name) and \
+         checkTask(cluster_name, service_name, task_name):
+        container_name  = setContainer(logger, cluster_name, task_name)
+      else :
+        raise Exception('正しいクラスター名またはサービス名またはタスク名を指定してください。')
+
+    ## cluster_name, service_name, task_name, container_nameの実在確認（最終）
+    if not checkCluster(cluster_name):
+      raise Exception('正しいクラスター名を指定してください。')
+    if not checkService(cluster_name, service_name):
+      raise Exception('正しいサービス名を指定してください。')
+    if not checkTask(cluster_name, service_name, task_name):
+      raise Exception('正しいタスク名を指定してください。')
+    if not checkContainer(cluster_name, task_name, container_name):
+      raise Exception('正しいコンテナ名を指定してください。')
+
+    ## Fargate接続関数を実行する
+    ecsExecute(logger, cluster_name, service_name, task_name, container_name, shell_cmd, logfile, force_connect)
+  except Exception as e:
+    logger.error("処理を終了します\nエラー詳細: {}\n{}".format(e, traceback.format_exc()))
+  return
+
+# 実行処理
+if __name__ == "__main__":
+  main()
+
